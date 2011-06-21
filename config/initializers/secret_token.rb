b2628f0f2f23e6a1e20d9531cc446f00e0a224f4
rubycs <rubycs@clair.ro>
2011-06-21 12:56:00 +0300

    New rails3.1 app added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heroku::Application.config.secret_token = '4ca9d87a5d94d4c0b0ad878a22cec6559011ef60dd70f2a4909086b958498a0968f48b98b274232633471a7b114263817adab0d8fe12b825994aa0ca566ef32f'
